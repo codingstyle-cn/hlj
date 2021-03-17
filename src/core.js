42d06e57964051e6a7971c1e63f6d9a988c3474c
jinhuisheng <509537277@qq.com>
2021-03-17 20:09:44 +0800

    Revert "add diff, and not, toEqual matchers"

--- src/core.js
+++ src/core.js
@@ -3,62 +3,37 @@ let totalCount = 0;
 let testCaseResults = [];
 
 const test = (name, callback) => {
-  totalCount++;
-  try {
-    callback();
-    testCaseResults.push({ name, isPassed: true });
-  } catch (e) {
-    testCaseResults.push({ name, isPassed: false });
-    throw e;
-  }
+    totalCount++;
+    try {
+        callback();
+        testCaseResults.push({name, isPassed: true});
+    } catch (e) {
+        testCaseResults.push({name, isPassed: false});
+        throw e;
+    }
 };
 
-const getToBe = (isNot) => (received) => (expected) => {
-  if (received !== expected && !isNot) {
-    throw new Error(JSON.stringify({ expected, received }));
-  }
-  passedCount++;
+const getToBe = actual => (expected) => {
+    if (actual !== expected) {
+        throw new Error("");
+    }
+    passedCount++;
 };
 
-const getToEqual = (isNot) => (received) => (expected) => {
-  const isObject = typeof received === 'object' && typeof expected === 'object';
-  const condition = isObject
-    ? JSON.stringify(received) !== JSON.stringify(expected)
-    : received !== expected;
-
-  if (condition && !isNot) {
-    throw new Error(JSON.stringify({ expected, received }));
-  }
-  passedCount++;
-};
-
-const expect = (received) => {
-  const matchers = (isNot = false) => ({
-    toBe: getToBe(isNot)(received),
-    toEqual: getToEqual(isNot)(received),
-  });
-  return {
-    ...matchers(),
-    not: { ...matchers(true) },
-  };
+const expect = (actual) => {
+    return {toBe: getToBe(actual)};
 };
 
 const getPassedCount = () => {
-  return passedCount;
+    return passedCount;
 };
 
 const getTotalCount = () => {
-  return totalCount;
+    return totalCount;
 };
 
 const getTestCaseResults = () => {
-  return testCaseResults;
+    return testCaseResults;
 };
 
-module.exports = {
-  test,
-  expect,
-  getPassedCount,
-  getTotalCount,
-  getTestCaseResults,
-};
+module.exports = {test, expect, getPassedCount, getTotalCount, getTestCaseResults};
